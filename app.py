b8b01421d726e53e2d6031dc7bb3a6241d523d8f
Emilia Tulan <emilia.tulan@gmail.com>
2021-04-30 10:18:30 +0200

    Append new greets

--- app.py
+++ app.py
@@ -9,31 +9,28 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/motd'
 db = SQLAlchemy(app)
 
-
-#add columns in the table
-class Name(db.Model):
+#create table and add columns
+class Greets(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
     message = db.Column(db.String(150), unique=True, nullable=False)
     image_file = db.Column(db.String(30), nullable=False, default='cat.jpg')
 
     def __repr__(self):
-      return f"Name('{self.message}', '{self.image_file}'"
-
-
-posts = [
-    {'message': 'It makes a big difference in your life when you stay positive.'},
-    {'message': 'You are off to great places, today is your day.'},
-    {'message': 'Live life to the fullest and focus on the positive.'},
-    {'message': 'If opportunity does not knock, build a door.'}
-]
-
+      return f"('{self.message}', '{self.name}', '{self.image_file}')"
 db.create_all()
+
+greets = [{'name': 'Simon', 'message': 'It makes a big difference in your life when you stay positive.'}, {'name': 'Bernhard', 'message': 'You are off to great places, today is your day.'}, {'name': 'Daniela', 'message': 'Live life to the fullest and focus on the positive.'}, {'name': 'Emilia', 'message': 'If opportunity does not knock, build a door.'},
+]
 
 # endpoint
 @app.route("/", methods=["GET", "POST",])
-def add_name():
-    return render_template('home.html', posts=posts)
+def create():
+  greets_new = {'name': 'Andreas', 'message': 'When life gives you lemons make a lemonade.'}
+  greets.append(greets_new)
+  return jsonify( greets)
+    # return render_template('home.html', greets=greets)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
