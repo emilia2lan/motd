18f89225462c633fe0e3de8f929dd81e6cb02ceb
Emilia Tulan <emilia.tulan@gmail.com>
2021-05-04 22:20:12 +0200

    change tables and refactor code

--- app/app.py
+++ app/app.py
@@ -1,3 +1,6 @@
+import json
+import random
+
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import Session, relationship
@@ -11,50 +14,69 @@ db = SQLAlchemy(app)
 session = Session()
 
 # creates two tables
-class c_names(db.Model):
-  __tablename__ = 'names_t'
-  name = db.Column(db.String(50), primary_key=True, nullable=False)
-  def __init__(self, name):
-    self.name = name
+class motd(db.Model):
+  __tablename__ = 'random_greeting'
+  greets = db.Column(db.String(), primary_key=True)
+  def __init__(self, greets):
+    self.greets = greets
 
-class c_greets(db.Model):
-  __tablename__ = 'greets_t'
-  name = db.Column(db.String(50), primary_key=True)
-  message = db.Column(db.String())
-  def __init__(self, name, message):
-    self.name = name
-    self.message = message
+class u_greets(db.Model):
+  __tablename__ = 'user_greeting'
+  user = db.Column(db.String(), primary_key=True)
+  greeting = db.Column(db.String())
+  def __init__(self, user, greeting):
+    self.user = user
+    self.greeting = greeting
 
 db.create_all()
 
-#insert multiple values in greets_t
-greets = [c_greets(name="Simon", message="Happiness is the only thing that multiplies when you share it."), c_greets(name="Bernhard", message="You are off to great places, today is your day."), c_greets(name="Daniela", message="Live life to the fullest and focus on the positive."), c_greets(name="Emilia", message="If opportunity does not knock, build a door."),c_greets(name="Cat", message="/cat.jpg")]
+
+#insert multiple values in the two tables
+greets = [u_greets(user="Simon", greeting="Happiness is the only thing that multiplies when you share it."), u_greets(user="Bernhard", greeting="You are off to great places, today is your day."), u_greets(user="Daniela", greeting="Live life to the fullest and focus on the positive."), u_greets(user="Emilia", greeting="If opportunity does not knock, build a door."),u_greets(user="Cat", greeting="/cat.jpg")]
+
+motd_list = [motd(greets="Welcome!"), motd(greets="How are you?"), motd(greets="Today is a good day!"), motd(greets="Nice to meet you!")]
 
 db.session.add_all(greets)
+db.session.add_all(motd_list)
 try:
   db.session.commit()
 except:
   db.session.close()
 
-
 @app.route('/api/v1/greet', methods=['POST'])
 def namesNew():
-# post method in names_t
-  namesData = request.get_json()
-  newName = c_names(name=namesData['name'])
-  db.session.add(newName)
-  db.session.commit()
+  namePost = request.get_json()
+  username = namePost["user"]
 
-#get all the values from greets_t
-  allNames = c_greets.query.all()
+#fetch all the values from user_greeting table
+  allUsers = u_greets.query.all()
   output = []
-  for name in allNames:
-    newName = {}
-    newName['name'] = name.name
-    newName['message']=name.message
-    output.append(newName)
+  for user in allUsers:
+    result = {}
+    result['user'] = user.user
+    result['greeting']=user.greeting
+    output.append(result)
 
-  return jsonify(namesData, output)
+#fetch all the values from random_greeting table
+  allGreets = motd.query.all()
+  result2 = []
+  for greets in allGreets:
+    quotes = {}
+    quotes['greets'] = greets.greets
+    result2.append(quotes)
+
+  # loops through user_greetings.user and check if the name inserted(post method) is in or not.
+  something = {}
+  for i in range(5):
+    something[output[i]["user"]] = output[i]["greeting"]
+
+  if username in something:
+    return username + something[username]
+    # if not returns the new user and gives a random message from random_greeting table
+  else:
+   return username + result2[random.randint(0, 3)]['greets']
+
+  return json.dumps({"msg": msg}, output, something, username, separators =(" "))
 
 if __name__ == '__main__':
   app.run(debug=True)
